ef420f7ff62fd21decc85658b9604d8a2994f387
Dhanush <dhanunanjunda2@gmail.com>
2020-06-19 16:45:35 -0700

    Updates

---
+++ precache-manifest.87d2963793e707cb5593f642dff3dfad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9d54930c185701a29f5d30457010bf",
+    "url": "/120-card-game/index.html"
+  },
+  {
+    "revision": "1b00bba134b0b5d5ff3f",
+    "url": "/120-card-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9c9c78f68f6fcb0985d",
+    "url": "/120-card-game/static/js/2.262ee91e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/120-card-game/static/js/2.262ee91e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b00bba134b0b5d5ff3f",
+    "url": "/120-card-game/static/js/main.232c6ee6.chunk.js"
+  },
+  {
+    "revision": "8772e502bc91a8436555",
+    "url": "/120-card-game/static/js/runtime-main.fd80cd30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/120-card-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
