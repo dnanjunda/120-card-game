e5baf18ac0fb7073c93cd50890b27438a19e7b88
Dhanush <dhanunanjunda2@gmail.com>
2020-06-23 23:33:20 -0700

    made files for each page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>Welcome to 120!</h1>
+         </div>
+        );
+    }
+}
+
+export default Home
---
+++ src/pages/Rules.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+
+class Rules extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> HOW TO PLAY: </h1>
+            </div>
+        );
+    }
+}
+
+export default Rules
