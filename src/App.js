7ef685be39dd5e70f80faf8b10a14ec9c26e9a6c
anoushkadave <57283134+anoushkadave@users.noreply.github.com>
2020-06-28 17:58:18 -0700

    try 6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import logo from './logo.svg';
 import React, { Component, View, Text } from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from './pages/Home';
 import Rules from './pages/Rules';
 import Leaderboard from './pages/Leaderboard';
-
 import Navigation from './components/Navigation';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
@@ -20,39 +13,11 @@ class App extends Component {
     return (
       <div>
         <Navigation/>
-        <Route path="" exact render={(props) => <Home {...props}/>}/>
+        <Route path="/120-card-game" exact render={(props) => <Home {...props}/>}/>
         <Route path="/" exact render={(props) => <Home {...props}/>}/>
 				<Route path="/home" exact render={(props) => <Home {...props}/>}/>
         <Route path="/rules" exact render={(props) => <Rules {...props}/>}/>
         <Route path="/leaderboard" exact render={(props) => <Leaderboard {...props}/>}/>
-
-     {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/rules">Rules</Link>
-            </li>
-            <li>
-              <Link to="/leaderboard">Leaderboard</Link>
-            </li>
-          </ul>
-     </nav> */}
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-       {/*<Switch>
-          <Route path="/rules">
-            <Rules />
-          </Route>
-          <Route path="/leaderboard">
-            <Leaderboard />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-       </Switch>*/}
       </div>
     );
   }
