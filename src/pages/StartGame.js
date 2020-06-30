609f25d192ad57b02c73778e2776873c81ca9db1
Dhanush <dhanunanjunda2@gmail.com>
2020-06-29 17:54:15 -0700

    home page changes added

--- src/pages/StartGame.js
+++ src/pages/StartGame.js
@@ -9,16 +9,16 @@ class StartGame extends React.Component {
             <div>
                 <Container>
                     <h1 className="Game-Title">Starting new game...</h1>
-                    <h1 className="Game-Code">Your game code is...</h1>
-                    <h2 className="Players"> Players in your game:</h2>
-                    <ol className="Players-List">
+                    <h1 className="Game-code">Your game code is...</h1>
+                    <h2 className="Players-Title"> Players in your game:</h2>
+                    <ol className="Players-list">
                         <li>Waiting</li>
                         <li>Waiting</li>
                         <li>Waiting</li>
                         <li>Waiting</li>
                         <li>Waiting</li>
                     </ol>
-                    <h2 className="Waiting">Waiting for five players to join...</h2>
+                    <h2 className="Waiting-Text">Waiting for five players to join...</h2>
                 </Container>
                 <br/>
                 <br/>
