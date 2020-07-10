861b346605fd216b68141fc7e50266e34fc51c5b
anoushkadave <57283134+anoushkadave@users.noreply.github.com>
2020-07-10 15:08:18 -0700

    made the seats

---
+++ frontend/src/components/Seat.js
@@ -0,0 +1,28 @@
+import React, { Component, View } from 'react';
+import { Button } from 'react-bootstrap';
+import '../App.css';
+import '../css/Seat.css';
+
+class Seat extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <svg width="280" height="250">
+                <circle
+                    cx={'140'}
+                    cy={'125'}
+                    r={'75'}
+                    fill={"#E1C699"}
+                />
+                <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="28px" font-family="Arial" dy=".3em">{this.props.playerName}</text>
+            </svg>
+        );
+    }
+}
+
+export default Seat;
--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import '../css/Game.css';
+import Seat from '../components/Seat.js';
 
 class Game extends React.Component {
 
@@ -28,14 +29,19 @@ class Game extends React.Component {
                 <head>
                     <script type="text/javascript" src="../../../backend/logic/Main.js"></script>
                 </head>
-                <h1 className='titleStyle'>Game</h1>
-                <p>array: {this.state.apiResponse.slice(1,18)}</p>
+                {/*<h1 className='Title-Style'>Game</h1>*/}
+                <Seat playerName={"Anoushka"}/>
+                <Seat playerName={"Dhanush"}/>
+                <Seat playerName={"Anshul"}/>
+                <Seat playerName={"Shreenithi"}/>
+                <Seat playerName={"Ashley"}/>
+                {/*<p>array: {this.state.apiResponse.slice(1,18)}</p>
                 <h1>{this.state.apiResponse.slice(1,18)}</h1>
                 <img src={require("../cards/KS.png")}></img>
-                {/*<img src={require(this.state.apiResponse.slice(1,18))}></img>*/}
+                <img src={require(this.state.apiResponse.slice(1,18))}></img>
                 <form method="post" action="http://localhost:9000/testAPI/start" >
                     <input type="submit" value="Start" />
-                </form>
+                </form> */}
             </div>
         );
     }
