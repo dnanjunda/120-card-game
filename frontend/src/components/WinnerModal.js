c179544a1d9b62a4546e83a822e5c9d299f79446
anoushkadave <57283134+anoushkadave@users.noreply.github.com>
2020-08-13 16:23:13 -0700

    more fixes

--- frontend/src/components/WinnerModal.js
+++ frontend/src/components/WinnerModal.js
@@ -23,30 +23,15 @@ class WinnerModal extends Component {
 
     render() {
 
-        if (this.state.visible && this.props.winningTeam === "Leading") return (
+        if (this.state.visible) return (
             <div className="Winner-Modal-Font">
                 <div className="Winner-Modal-Container">
-                    <div className="Winner-Modal-Leading-Text">
-                        {this.props.winningTeam} Team ({this.props.winningPlayers[0]}, {this.props.winningPlayers[1]}) won with {this.props.winningTotal} points!
+                    <div className="Winner-Modal-Text">
+                        {this.props.winningTeam} Team won with {this.props.winningTotal} points!
                     </div>
                     <div>
                         <div className="Winner-Modal-Buttons">
-                            <button onClick={this.handleCloseClick} className="Winner-Modal-Leading-Close-Button">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-
-        else if (this.state.visible && this.props.winningTeam === "Defending") return (
-            <div className="Winner-Modal-Font">
-                <div className="Winner-Modal-Container">
-                    <div className="Winner-Modal-Defending-Text">
-                        {this.props.winningTeam} Team ({this.props.winningPlayers[0]}, {this.props.winningPlayers[1]}, {this.props.winningPlayers[2]}) won with {this.props.winningTotal} points!
-                    </div>
-                    <div>
-                        <div className="Winner-Modal-Buttons">
-                            <button onClick={this.handleCloseClick} className="Winner-Modal-Defending-Close-Button">Close</button>
+                            <button onClick={this.handleCloseClick} className="Winner-Modal-Close-Button">Close</button>
                         </div>
                     </div>
                 </div>
