77cc219e33fa773a253698eaa283f3bd34b1f70e
anoushkadave <57283134+anoushkadave@users.noreply.github.com>
2020-08-15 04:41:57 -0700

    added box shadow

--- frontend/src/pages/Results.js
+++ frontend/src/pages/Results.js
@@ -50,191 +50,189 @@ class Results extends React.Component {
                     winningTotal={this.state.winningTotal}
                 />
 
-                <div className="Results-Container">
-                    <div className="Rows">
-                        <Col className="Title">
-                            Player
+                <div className="Rows">
+                    <Col className="Title">
+                        Player
                         </Col>
-                        <Col className="Title">
-                            Cards
+                    <Col className="Title">
+                        Cards
                         </Col>
-                        <Col className="Title">
-                            Points
+                    <Col className="Title">
+                        Points
                         </Col>
-                        <Col className="Title">
-                            Total Points
+                    <Col className="Title">
+                        Total Points
                         </Col>
-                        <Col className="Title">
-                            Games Won
+                    <Col className="Title">
+                        Games Won
                         </Col>
-                        <Col className="Title">
-                            Rank
+                    <Col className="Title">
+                        Rank
                         </Col>
-                    </div>
-
-                    <div className="Rows">
-                        <Col className="Player">
-                            {this.state.players[0]} <span>{'\u{1F451}'}</span>
-                        </Col>
-                        <Col className="Player">
-                            <div className="Card-Scroll">
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                            </div>
-                        </Col>
-                        <Col className="Player">
-                            50
-                        </Col>
-                        <Col className="Player">
-                            1000
-                        </Col>
-                        <Col className="Player">
-                            10
-                        </Col>
-                        <Col className="Player">
-                            1
-                        </Col>
-                    </div>
-
-                    <div className="Rows">
-                        <Col className="Player">
-                            {this.state.players[1]}
-                        </Col>
-                        <Col className="Player">
-                            <div className="Card-Scroll">
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                            </div>
-                        </Col>
-                        <Col className="Player">
-                            10
-                        </Col>
-                        <Col className="Player">
-                            1000
-                        </Col>
-                        <Col className="Player">
-                            10
-                        </Col>
-                        <Col className="Player">
-                            3
-                        </Col>
-                    </div>
-
-                    <div className="Rows">
-                        <Col className="Player">
-                            {this.state.players[2]}
-                        </Col>
-                        <Col className="Player">
-                            <div className="Card-Scroll">
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                            </div>
-                        </Col>
-                        <Col className="Player">
-                            10
-                        </Col>
-                        <Col className="Player">
-                            1000
-                        </Col>
-                        <Col className="Player">
-                            10
-                        </Col>
-                        <Col className="Player">
-                            5
-                        </Col>
-                    </div>
-
-                    <div className="Rows">
-                        <Col className="Player">
-                            {this.state.players[3]} <span>{'\u{1F451}'}</span>
-                        </Col>
-                        <Col className="Player">
-                            <div className="Card-Scroll">
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                            </div>
-                        </Col>
-                        <Col className="Player">
-                            30
-                        </Col>
-                        <Col className="Player">
-                            1000
-                        </Col>
-                        <Col className="Player">
-                            10
-                        </Col>
-                        <Col className="Player">
-                            2
-                        </Col>
-                    </div>
-
-                    <div className="Rows">
-                        <Col className="Player">
-                            {this.state.players[4]} 
-                        </Col>
-                        <Col className="Player">
-                            <div className="Card-Scroll">
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                                <img className="Card" src={require("../cards/AS.png")}></img>
-                            </div>
-                        </Col>
-                        <Col className="Player">
-                            20
-                        </Col>
-                        <Col className="Player">
-                            1000
-                        </Col>
-                        <Col className="Player">
-                            10
-                        </Col>
-                        <Col className="Player">
-                            4
-                        </Col>
-                    </div>
-
-                    <FooterButtons
-                        leftButtonTitle="Start Next Game"
-                        leftPath="/game"
-                        rightButtonTitle="End Game"
-                        rightPath="/home"
-                        className="Results"
-                    />
                 </div>
+
+                <div className="Rows">
+                    <Col className="Player">
+                        {this.state.players[0]} <span>{'\u{1F451}'}</span>
+                    </Col>
+                    <Col className="Player">
+                        <div className="Card-Scroll">
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                        </div>
+                    </Col>
+                    <Col className="Player">
+                        50
+                        </Col>
+                    <Col className="Player">
+                        1000
+                        </Col>
+                    <Col className="Player">
+                        10
+                        </Col>
+                    <Col className="Player">
+                        1
+                        </Col>
+                </div>
+
+                <div className="Rows">
+                    <Col className="Player">
+                        {this.state.players[1]}
+                    </Col>
+                    <Col className="Player">
+                        <div className="Card-Scroll">
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                        </div>
+                    </Col>
+                    <Col className="Player">
+                        10
+                        </Col>
+                    <Col className="Player">
+                        1000
+                        </Col>
+                    <Col className="Player">
+                        10
+                        </Col>
+                    <Col className="Player">
+                        3
+                        </Col>
+                </div>
+
+                <div className="Rows">
+                    <Col className="Player">
+                        {this.state.players[2]}
+                    </Col>
+                    <Col className="Player">
+                        <div className="Card-Scroll">
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                        </div>
+                    </Col>
+                    <Col className="Player">
+                        10
+                        </Col>
+                    <Col className="Player">
+                        1000
+                        </Col>
+                    <Col className="Player">
+                        10
+                        </Col>
+                    <Col className="Player">
+                        5
+                        </Col>
+                </div>
+
+                <div className="Rows">
+                    <Col className="Player">
+                        {this.state.players[3]} <span>{'\u{1F451}'}</span>
+                    </Col>
+                    <Col className="Player">
+                        <div className="Card-Scroll">
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                        </div>
+                    </Col>
+                    <Col className="Player">
+                        30
+                        </Col>
+                    <Col className="Player">
+                        1000
+                        </Col>
+                    <Col className="Player">
+                        10
+                        </Col>
+                    <Col className="Player">
+                        2
+                        </Col>
+                </div>
+
+                <div className="Rows">
+                    <Col className="Player">
+                        {this.state.players[4]}
+                    </Col>
+                    <Col className="Player">
+                        <div className="Card-Scroll">
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                            <img className="Card" src={require("../cards/AS.png")}></img>
+                        </div>
+                    </Col>
+                    <Col className="Player">
+                        20
+                        </Col>
+                    <Col className="Player">
+                        1000
+                        </Col>
+                    <Col className="Player">
+                        10
+                        </Col>
+                    <Col className="Player">
+                        4
+                        </Col>
+                </div>
+
+                <FooterButtons
+                    leftButtonTitle="Start Next Game"
+                    leftPath="/game"
+                    rightButtonTitle="End Game"
+                    rightPath="/home"
+                    className="Results"
+                />
             </div>
         );
     }
