94da1f8b369e243c240b32de35460be54539c0d4
anoushkadave <57283134+anoushkadave@users.noreply.github.com>
2020-08-20 16:28:34 -0700

    fixing conflicts

--- backend/logic/Main.js
+++ backend/logic/Main.js
@@ -1,4 +1,5 @@
 const prompt = require('prompt');
+prompt.start();
 
 class Card {
 
@@ -117,7 +118,6 @@ class Game {
         this.leadingBid = 70;
         this.cuttingSuit;
         this.partnerCard;
-        this.handStarter;
         this.winners = []; // winners of current game
     }
 
@@ -525,25 +525,6 @@ class Game {
         return true;
     }
 
-    promptUser() {
-        prompt.start();
-
-        prompt.get(['bid'], function(err, result) {
-            if (err) {
-                return onErr(err);
-            }
-            //this.players[currentBidder].playerBid = result.bid;
-            //console.log(this.players[currentBidder].playerName, result.bid);
-            console.log(result.bid);
-            return result.bid;
-        });
-
-        function onErr(err) {
-            console.log(err);
-            return 1;
-        }
-    }
-
     // bidding method, also sets leader, cutting suit, and partner card
     setLeader() {
 
@@ -552,7 +533,6 @@ class Game {
 
         let leadingPlayerIndex = this.dealer;
         let leadingBid = 70;
-        
 
         while (bidders > 1) {
 
@@ -561,21 +541,24 @@ class Game {
                 break;
             }
 
-            this.players[currentBidder].playerBid = this.promptUser();
-
-            console.log(this.players[currentBidder].playerName);
-            console.log(this.players[currentBidder].playerBid);
+            prompt.get(['bid'], function(err, result) {
+                if (err) {
+                    console.log(err);
+                    return 1;
+                }
+                //this.players[currentBidder].playerBid = result.bid;
+                //console.log(this.players[currentBidder].playerName, result.bid);
+                console.log(result.bid);
+            })
 
             if (this.players[currentBidder].playerBid > leadingBid) {
                 leadingBid = this.players[currentBidder].playerBid;
                 leadingPlayerIndex = currentBidder;
-                console.log("entered if");
             }
 
             else {
                 this.players[currentBidder].bidComplete = true;
                 bidders--;
-                console.log("entered else");
             }
 
             while (this.players[currentBidder].bidComplete) {
@@ -603,15 +586,6 @@ class Game {
                 }
             }
         }
-    }
-
-    handContains(player, suit) {
-        for(let i = 0; i < this.players[player].playerStack.length; i++) {
-            if(this.players[player].playerStack[i].suit === suit) {
-                return true;
-            }
-        }
-        return false;
     }
 
     // must set card precendence based on cutting suit and which card was played first for each hand
@@ -724,9 +698,6 @@ class Game {
         }
 
         this.players[handWinner].playerHands.push(hand);
-
-        //sets player who starts next hand
-        this.handStarter = this.players[handWinner];
     }
 
     // determines winner
@@ -856,7 +827,7 @@ class Game {
     // must implement logic for who gets to start the next hand 
     // must announce who partners are when the partner card gets played (revealed)
     // must implement logic for who won each game based on points
-    // after each game, must update ranks & update dealer and start next game, clear all last game info
+    // after each game, must update ranks (ties) & update dealer and start next game, clear all last game info
     // august 17-24 finish logic
     // august 24-31 finish frontend to test logic, finish codes stuff
     // september get provisional patent, server setup, deploy
@@ -934,8 +905,6 @@ p5StartingCards.push(game.players[4].playerStack[8].image);
 p5StartingCards.push(game.players[4].playerStack[9].image);
 
 game.setLeader();
-
-//game.totalPoints();
 
 // hardcoded what card they played
 hand = [[playerOne, playerOne.playerStack[0]], [playerTwo, playerTwo.playerStack[0]], [playerThree, playerThree.playerStack[0]], 
