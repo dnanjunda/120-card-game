13451f7bd3f1706173a9274f82701cdccb513c2b
anoushkadave <57283134+anoushkadave@users.noreply.github.com>
2020-08-30 16:34:03 -0700

    bidding setup complete

--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -30,7 +30,8 @@ class Game extends React.Component {
             dealer: "",
             cutting: "",
             partner: "",
-            isBidding: true,
+            isBidding: false,
+            biddingComplete: false,
             currentBidder: "",
         };
     }
@@ -59,6 +60,7 @@ class Game extends React.Component {
             cutting: "AS",
             partner: "TWOS",
             isBidding: true,
+            biddingComplete: false,
             currentBidder: "Anoushka",
         })
     }
@@ -67,18 +69,24 @@ class Game extends React.Component {
 
         let textOnTable;
 
-        if ((this.state.isBidding)) {
-            {/*Only want this text if bidding is going on.*/ }
-            textOnTable = <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="2vw" font-family="American Typewriter, serif" dy=".3em">Waiting for {this.state.currentBidder} to bid...</text>;
-            {/* need to figure out how to make this go away after (change isBidding to false) */ }
+        if (!(this.state.biddingComplete)) {
+            if (!(this.state.isBidding)) {
+                {/*Only want this text if bidding is going on.*/ }
+                textOnTable = <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="2vw" font-family="American Typewriter, serif" dy=".3em" z-index="5">Waiting for {this.state.currentBidder} to bid...</text>;
+                {/* need to figure out how to make this go away after (change isBidding to false) */ }
+            }
+    
+            else {
+                textOnTable = <BiddingPopup
+                    isBidding={this.state.isBidding}
+                    minBidAvailable="70"
+                    player="Anoushka"
+                />;
+            }
         }
 
         else {
-            textOnTable = <BiddingPopup
-                isBidding={this.state.isBidding}
-                minBidAvailable="70"
-                player="Anoushka"
-            />;
+            textOnTable = <div></div>;
         }
 
         return (
@@ -87,74 +95,72 @@ class Game extends React.Component {
                     <head>
                         <script type="text/javascript" src="../../../backend/logic/Main.js"></script>
                     </head>
-                    <BiddingPopup
-                        isBidding={this.state.isBidding}
-                        minBidAvailable="70"
-                        player="Anoushka"
-                        />
                     <Row>
                         <Col>
-                        {/* Seats */}
-                        <Seat playerName={this.state.players[1]} className={"Player-Two"} />
-                        <Seat playerName={this.state.players[2]} className={"Player-Three"} />
-                        <Seat playerName={this.state.players[3]} className={"Player-Four"} />
-                        <Seat playerName={this.state.players[4]} className={"Player-Five"} />
+                            {/* Seats */}
+                            <Seat playerName={this.state.players[1]} className={"Player-Two"} />
+                            <Seat playerName={this.state.players[2]} className={"Player-Three"} />
+                            <Seat playerName={this.state.players[3]} className={"Player-Four"} />
+                            <Seat playerName={this.state.players[4]} className={"Player-Five"} />
 
-                        {/*Table*/}
-                        <svg width="450" height="450" className="Table">
-                            <circle
-                                cx={'225'}
-                                cy={'225'}
-                                r={'225'}
-                                fill={"#391f03"}
-                            />
-                            {textOnTable}
-                        </svg>
+                            {/*Table*/}
+                            <div>
+                                <svg width="450" height="450" className="Table">
+                                    <circle
+                                        cx={'225'}
+                                        cy={'225'}
+                                        r={'225'}
+                                        fill={"#391f03"}
+                                    />
+                                    {textOnTable}
+                                </svg>
+                                {textOnTable}
+                            </div>
 
-                        <Row>
-                            <Card className="Card-Image-One" test={this.state.playerCardImages[0]} />
-                            <Card className="Card-Image-Two" test={this.state.playerCardImages[1]} />
-                            <Card className="Card-Image-Three" test={this.state.playerCardImages[2]} />
-                            <Card className="Card-Image-Four" test={this.state.playerCardImages[3]} />
-                            <Card className="Card-Image-Five" test={this.state.playerCardImages[4]} />
-                            <Card className="Card-Image-Six" test={this.state.playerCardImages[5]} />
-                            <Card className="Card-Image-Seven" test={this.state.playerCardImages[6]} />
-                            <Card className="Card-Image-Eight" test={this.state.playerCardImages[7]} />
-                            <Card className="Card-Image-Nine" test={this.state.playerCardImages[8]} />
-                            <Card className="Card-Image-Ten" test={this.state.playerCardImages[9]} />
-                        </Row>
-                    </Col>
-                    <Col>
-                        <Row>
-                            <Col>
-                                <GameButton buttonText='Leader' text={this.state.leader} className="Leader-Button" />
-                            </Col>
-                            <Col>
-                                <GameButton buttonText='Bid' text={this.state.bid} className="Bid-Button" />
-                            </Col>
-                            <Col>
-                                <GameButton buttonText='Dealer' text={this.state.dealer} className="Dealer-Button" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <h1 className="Current-Cutting">Cutting Suit</h1>
-                                <Card className="Current-Cutting-Images" test={this.state.cutting} />
-                            </Col>
-                            <Col>
-                                <h1 className="Current-Partner">Partner Card</h1>
-                                <Card className="Current-Partner-Images" test={this.state.partner} />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <FooterButtons
-                                buttonsCount="1"
-                                buttonTitle="End Game"
-                                buttonPath="/results"
-                                className="Game"
-                            />
-                        </Row>
-                    </Col>
+                            <Row>
+                                <Card className="Card-Image-One" test={this.state.playerCardImages[0]} />
+                                <Card className="Card-Image-Two" test={this.state.playerCardImages[1]} />
+                                <Card className="Card-Image-Three" test={this.state.playerCardImages[2]} />
+                                <Card className="Card-Image-Four" test={this.state.playerCardImages[3]} />
+                                <Card className="Card-Image-Five" test={this.state.playerCardImages[4]} />
+                                <Card className="Card-Image-Six" test={this.state.playerCardImages[5]} />
+                                <Card className="Card-Image-Seven" test={this.state.playerCardImages[6]} />
+                                <Card className="Card-Image-Eight" test={this.state.playerCardImages[7]} />
+                                <Card className="Card-Image-Nine" test={this.state.playerCardImages[8]} />
+                                <Card className="Card-Image-Ten" test={this.state.playerCardImages[9]} />
+                            </Row>
+                        </Col>
+                        <Col>
+                            <Row>
+                                <Col>
+                                    <GameButton buttonText='Leader' text={this.state.leader} className="Leader-Button" />
+                                </Col>
+                                <Col>
+                                    <GameButton buttonText='Bid' text={this.state.bid} className="Bid-Button" />
+                                </Col>
+                                <Col>
+                                    <GameButton buttonText='Dealer' text={this.state.dealer} className="Dealer-Button" />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <h1 className="Current-Cutting">Cutting Suit</h1>
+                                    <Card className="Current-Cutting-Images" test={this.state.cutting} />
+                                </Col>
+                                <Col>
+                                    <h1 className="Current-Partner">Partner Card</h1>
+                                    <Card className="Current-Partner-Images" test={this.state.partner} />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <FooterButtons
+                                    buttonsCount="1"
+                                    buttonTitle="End Game"
+                                    buttonPath="/results"
+                                    className="Game"
+                                />
+                            </Row>
+                        </Col>
                     </Row>
                 </Container>
             </div >
